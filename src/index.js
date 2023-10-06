e45514903907ed4b7325740383fcefdd99a193ab
NormapGarciao <normapgarciao@gmail.com>
2023-10-05 19:27:30 -0500

    realizado requerimientos minimos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
+//obtiene el texto que el usuario escribe en uel área de texto
 function contador1() {
   const text = textarea.value;
+  //actualiza el contenido del elemento en la página web usando el objeto analyzer
   document.querySelector('[data-testid="word-count"]').innerHTML = 'recuento de palabras ' + analyzer.getWordCount(text);
   document.querySelector('[data-testid="character-count"]').innerHTML = 'recuento de caracteres ' + analyzer.getCharacterCount(text);
   document.querySelector('[data-testid="character-no-spaces-count"]').innerHTML = 'recuento de caracteres excluyendo espacios y signos de puntuacion ' + analyzer.getCharacterCountExcludingSpaces(text);
@@ -13,10 +15,11 @@ function contador1() {
 const textarea = document.querySelector("textarea[name='user-input']");
 const resetButton = document.getElementById("reset-button");
 
-resetButton.addEventListener("click", () => {
+//boton de limpiar y restrablece a 0
+resetButton.addEventListener("click", () => {  
   textarea.value = "";
   contador1(); 
-  
+//actualiza el contenido de los elementos con el atributo data-testid 
   document.querySelector('[data-testid="word-count"]').innerHTML = 'recuento de palabras ' + 0;
   document.querySelector('[data-testid="character-count"]').innerHTML = 'recuento de caracteres ' + 0;
   document.querySelector('[data-testid="character-no-spaces-count"]').innerHTML = 'recuento de caracteres excluyendo espacios y signos de puntuacion ' + 0;
@@ -25,7 +28,7 @@ resetButton.addEventListener("click", () => {
   document.querySelector('[data-testid="word-length-average"]').innerHTML = 'longitud media de palabras ' + 0;
 
 });
-textarea.addEventListener("input", contador1);
+textarea.addEventListener("input", contador1); //escucha los eventos como escribir y borrar
 
 
 
